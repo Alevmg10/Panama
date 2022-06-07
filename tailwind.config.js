4d79b2847631e0e00515875069ecdb38a4529777
Alejandro <mendozag.alejandro@gmail.com>
2022-06-07 19:10:48 -0400

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primaryColor: '#ff6600',
+        secondaryColor: '#4885ed',
+        black: '#212121',
+        lightGrey: '#f8f8f8',
+      },
+    },
+  },
+  plugins: [],
+}
